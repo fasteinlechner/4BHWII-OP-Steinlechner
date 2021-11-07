700fc5dde9aa842a30cff0e1d604c7741ce687e1
Fabian Steinlechner <fasteinlechner@tsn.at>
2021-11-07 11:19:34 +0100

    Endrekursion

--- Aufgabe-2-Summenfunktion-Hochfunktion/Summenfunktion_Hochfunktion.java
+++ Aufgabe-2-Summenfunktion-Hochfunktion/Summenfunktion_Hochfunktion.java
@@ -4,10 +4,12 @@ public class Summenfunktion_Hochfunktion{
         //Aufruf der Summenfunktion auf beide Arten
         System.out.println(sumIter(100));
         System.out.println(sumRek(100));
+        System.out.println(sumEndRek(0,100));
 
         //Aufruf der Hochfunktion auf beide Arten
         System.out.println(hochIter(3,3));
         System.out.println(hochRek(3,3));
+        System.out.println(hochEndRek(1,3, 3));
 
         
     }
@@ -29,18 +31,32 @@ public class Summenfunktion_Hochfunktion{
         return num+sumRek(num-1);
     }
 
-    public static int hochIter(int a,int num){
-        int result=a;
-        for(int i=1; i<num; i++){
-            result*=a;
+    public static int sumEndRek(int res, int num){
+        if (num<=0) {
+            return res;
+        }
+        return sumEndRek(res+num, num-1);
+    }
+
+    public static int hochIter(int num,int hoch){
+        int result=num;
+        for(int i=1; i<hoch; i++){
+            result*=num;
         }
         return result;
     }
 
-    public static int hochRek(int a, int num){
-        if (num==0){
+    public static int hochRek(int num, int hoch){
+        if (hoch==0){
             return 1;
         }
-        return a* hochRek(a,num-1);
+        return num* hochRek(num,hoch-1);
+    }
+    
+    public static int hochEndRek(int res, int num, int hoch){
+        if(hoch==0){
+            return res;
+        }
+        return hochEndRek(res*num,num, hoch-1);
     }
 }
